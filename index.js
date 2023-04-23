0d029e53689463369419228b10179cac7bc04bcf
Faiem-qy <f.quanyoung@gmail.com>
2023-04-23 14:54:28 -0400

    modified to include flyover times

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // // index.js
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -13,10 +13,24 @@ fetchMyIP((error, ip) => {
       console.log("It didn't work!", error);
       return;
     }
-
     console.log('It worked! Returned coordinates:', coordinates);
+
+    fetchISSFlyOverTimes(coordinates, (error, flyOverTimes) => {
+      if (error) {
+        console.log("It didn't work!", error);
+        return;
+      }
+      console.log('It worked! Returned flyOverTimes:', flyOverTimes);
+    });
   });
+  nextISSTimesForMyLocation((error, passTimes) => {
+    if (error) {
+      return console.log("It didn't work!", error);
+    }
+    // success, print out the deets!
+    console.log(passTimes);
+  });
+
 
 });
 
-// let ip = "99.211.180.41";
